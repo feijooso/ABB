2b7cf7bb8fa5cb70e1b1df2934a5e33b8bdb744d
Franco Alonso <francoXavierAlonso@gmail.com>
2018-11-09 02:15:11 -0300

    buscar_nodo
    -Added the tests for abb_nuevo() and abb_guardar().
    -Fixed some logic in nodo_buscar().

--- abb.c
+++ abb.c
@@ -22,10 +22,16 @@ void destruir_dato(void* dato) {
 	free(dato);
 }
 
-abb_nodo_t* crear_nodo(const char* clave, void* dato){
+abb_nodo_t* crear_nodo(const char* clave, void* dato) {
 	abb_nodo_t* nodo = malloc(sizeof(abb_nodo_t));
-	char* copia_clave = NULL;
-	strcpy(copia_clave, clave);
+	if(nodo == NULL) return NULL;
+	size_t largo = strlen(clave);
+	char* copia_clave = malloc(sizeof(char) * largo);
+	if(copia_clave == NULL) return NULL;
+	for(int i=0; i<largo; i++) {
+		copia_clave[i] = clave[i];
+	}
+	copia_clave[largo] = '\0';
 	nodo->clave = copia_clave;
 	nodo->dato = dato;
 	nodo->izq = NULL;
@@ -34,26 +40,24 @@ abb_nodo_t* crear_nodo(const char* clave, void* dato){
 }
 
 abb_nodo_t* buscar_nodo(const abb_t* arbol, abb_nodo_t* nodo, const char* clave) {
-	if (nodo == NULL) return NULL;
-	if (arbol->cmp(clave, nodo->clave) == 0) return nodo;
-	if (arbol->cmp(clave, nodo->clave) > 0) return buscar_nodo(arbol, nodo->der, clave);
+	if(nodo == NULL || arbol->cmp(clave, nodo->clave) == 0) return nodo;
+	if(arbol->cmp(clave, nodo->clave) > 0) return buscar_nodo(arbol, nodo->der, clave);
 	return buscar_nodo(arbol, nodo->izq, clave);
 }
 
 // si no tiene padre, se devuelve a si mismo
-abb_nodo_t* buscar_padre(const abb_t* arbol, abb_nodo_t* nodo, const char* clave){
-	if((nodo->izq == NULL && nodo->der == NULL) || (arbol->cmp(clave, nodo->clave) == 0)) return nodo;
-	if(arbol->cmp(clave, nodo->clave) > 0){
+abb_nodo_t* buscar_padre(const abb_t* arbol, abb_nodo_t* nodo, const char* clave) {
+	if(nodo == NULL || (nodo->izq == NULL && nodo->der == NULL) || (arbol->cmp(clave, nodo->clave) == 0)) return nodo;
+	if(arbol->cmp(clave, nodo->clave) > 0) {
 		if(nodo->der == NULL) return nodo;
 		return buscar_padre(arbol, nodo->der, clave);
-	}
-	else{
+	} else {
 		if(nodo->izq == NULL) return nodo;
 		return buscar_padre(arbol, nodo->izq, clave);
 	}
 }
 
-size_t cantidad_hijos(abb_nodo_t* nodo){
+size_t cantidad_hijos(abb_nodo_t* nodo) {
 	size_t cantidad = 0;
 	if(!nodo->izq) cantidad++;
 	if(!nodo->der) cantidad++;
@@ -61,9 +65,7 @@ size_t cantidad_hijos(abb_nodo_t* nodo){
 }
 
 /* PRIMITIVAS ABB*/
-
-
-abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato) {
 	abb_t* arbol = malloc(sizeof(abb_t));
 	if (arbol == NULL) return NULL;
 	arbol->raiz = NULL;
@@ -73,19 +75,15 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
 	return arbol;
 }
 
-bool abb_guardar(abb_t* arbol, const char* clave, void* dato){ //habria que reutilizar mas codigo, pero no se como
+bool abb_guardar(abb_t* arbol, const char* clave, void* dato) { //habria que reutilizar mas codigo, pero no se como
 	abb_nodo_t* nodo_nuevo = crear_nodo(clave, dato);
 	if (nodo_nuevo == NULL) return false;
-	if (arbol->raiz == NULL) {
+	if (arbol->raiz == NULL) 
 		arbol->raiz = nodo_nuevo;
-	} else {
+	else {
 		abb_nodo_t* nodo = buscar_padre(arbol, arbol->raiz, clave);
-		if(arbol->cmp(clave, nodo->clave) == 0){
-			nodo->dato = dato;
-		}
-		else if (arbol->cmp(clave, nodo->clave) > 0){
-			nodo->der = nodo_nuevo;
-		}
+		if(arbol->cmp(clave, nodo->clave) == 0) nodo->dato = dato;
+		else if(arbol->cmp(clave, nodo->clave) > 0) nodo->der = nodo_nuevo;
 		else nodo->izq = nodo_nuevo;
 	}
 	arbol->cantidad++;
@@ -94,6 +92,7 @@ bool abb_guardar(abb_t* arbol, const char* clave, void* dato){ //habria que reut
 
 void* abb_borrar(abb_t* arbol, const char* clave){
 	abb_nodo_t* nodo_a_borrar = buscar_nodo(arbol, arbol->raiz, clave);
+	if(nodo_a_borrar == NULL) return NULL;
 	abb_nodo_t* padre = buscar_padre(arbol, arbol->raiz,clave);
 	size_t cant_hijos = cantidad_hijos(nodo_a_borrar);
 	//if(arbol->cmp(padre->clave, clave) == 0) // es raiz
@@ -104,24 +103,21 @@ void* abb_borrar(abb_t* arbol, const char* clave){
 
 	if(cant_hijos == 1){
 		if(arbol->cmp(padre->clave, nodo_a_borrar->clave)){ //el nodo esta a la izq del padre
-			if(!nodo_a_borrar->izq){ //el hijo q tiene es a la izq 
+			if(!nodo_a_borrar->izq) { //el hijo q tiene es a la izq 
 				padre->izq = nodo_a_borrar->izq;
 			}
 			else padre->izq = nodo_a_borrar->der; //el hijo q tiene es a la der
-		
-		}
-		else{ //el nodo esta a la der del padre
-			if(!nodo_a_borrar->izq){ //el hijo q tiene es a la izq 
+		} else { //el nodo esta a la der del padre
+			if(!nodo_a_borrar->izq) { //el hijo q tiene es a la izq 
 				padre->der = nodo_a_borrar->izq;
 			}
 			else padre->der = nodo_a_borrar->der; //el hijo q tiene es a la der
-
 		}
 	}
 
 	if(cant_hijos == 2){
 		abb_nodo_t* nieto = nodo_a_borrar->der; //voy a la der
-		while(!nieto->izq){ //avanzo a la izq hasta el fondo
+		while(!nieto->izq) { //avanzo a la izq hasta el fondo
 			nieto = nieto->izq;
 		}
 		padre->der = nieto;
@@ -131,12 +127,14 @@ void* abb_borrar(abb_t* arbol, const char* clave){
 }
 //devuelve null si no esta en el arbol
 
-void* abb_obtener(const abb_t* arbol, const char* clave){
-	return buscar_nodo(arbol, arbol->raiz, clave);
+void* abb_obtener(const abb_t* arbol, const char* clave) {
+	abb_nodo_t* nodo = buscar_nodo(arbol, arbol->raiz, clave);
+	if(nodo == NULL) return NULL;
+	return nodo->dato; 
 }
 
 bool abb_pertenece(const abb_t* arbol, const char* clave){
-	return (buscar_nodo(arbol, arbol->raiz, clave));
+	return buscar_nodo(arbol, arbol->raiz, clave) != NULL;
 }
 
 size_t abb_cantidad(abb_t* arbol){
@@ -144,7 +142,7 @@ size_t abb_cantidad(abb_t* arbol){
 }
 
 void abb_destruir(abb_t* arbol){
-
+	free(arbol);
 }
 
 
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,6 +1,35 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "abb.h"
 #include "testing.h"
 
-void pruebas_abb_alumno() {
+void prueba_abb_vacio() {
+    abb_t* abb = abb_crear(strcmp, NULL);
+    print_test("cantidad = 0", abb_cantidad(abb) == 0);
+    print_test("borrar = NULL", abb_borrar(abb, "") == NULL);
+    print_test("pertenece = NULL", abb_pertenece(abb, "") == false);
+    abb_destruir(abb);
+}
 
+void prueba_abb_guardar() {
+    abb_t* abb = abb_crear(strcmp, NULL);
+    const char* menor = "1111";
+    const char* medio = "2222";
+    const char* mayor = "3333";
+    int valor1 = 3;
+    abb_guardar(abb, medio, NULL);
+    abb_guardar(abb, menor, &valor1);
+    abb_guardar(abb, mayor, NULL);
+    print_test("cantidad == 3", abb_cantidad(abb) == 3);
+    print_test("menor pertenece", abb_pertenece(abb, menor));
+    print_test("menor obtener", abb_obtener(abb, menor) == &valor1);
+    print_test("no pertenece random", abb_pertenece(abb, "RANDOM") == false);
+    abb_destruir(abb);
+}
+
+void pruebas_abb_alumno() {
+    //prueba_abb_vacio();
+    prueba_abb_guardar();
 }
